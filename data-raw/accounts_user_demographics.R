654b8baa6c47f4f331e887b1862771128e781ac2
Richard Sprague <richardsprague@gmail.com>
2021-11-30 13:05:02 -0800

    add 'age' field to accounts_user_demographics

--- data-raw/accounts_user_demographics.R
+++ data-raw/accounts_user_demographics.R
@@ -1,4 +1,4 @@
 ## code to prepare `accounts_user_demographics` dataset goes here
-accounts_user_demographics <- read.csv(system.file("extdata", package = "cgmrdb", "sample_accounts_user_demographics.csv"))
+accounts_user_demographics <- readr::read_csv(system.file("extdata", package = "cgmrdb", "sample_accounts_user_demographics.csv"))
 
 usethis::use_data(accounts_user_demographics, overwrite = TRUE)
