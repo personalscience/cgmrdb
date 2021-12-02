67641133a3e6583ee48e82a9426a440a1bb747a1
Richard Sprague <richardsprague@gmail.com>
2021-12-02 12:56:38 -0800

    add modified/created fields to accounts_firebase

--- data-raw/accounts_firebase.R
+++ data-raw/accounts_firebase.R
@@ -1,4 +1,11 @@
 ## code to prepare `accounts_firebase` dataset goes here
 
-accounts_firebase <- read.csv(system.file("extdata", package = "cgmrdb", "sample_accounts_firebase.csv"))
+accounts_firebase <- readr::read_csv(system.file("extdata", package = "cgmrdb", "sample_accounts_firebase.csv"),
+                                     col_types = cols(
+                                       id = col_double(),
+                                       user_id = col_double(),
+                                       firebase_id = col_character(),
+                                       created = col_datetime(),
+                                       modified = col_datetime()
+                                     ))
 usethis::use_data(accounts_firebase, overwrite = TRUE)
